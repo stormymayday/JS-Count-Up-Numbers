da315f6213f86b10aecfa63ae24bfeae7c587a1d
stormymayday <stormymayday7@gmail.com>
2023-04-12 21:44:08 -0600

    Moved updateCount above the function call to avoid the ReferenceError

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 // Selecting all '.number' elements and converting them into an array
 const items = [...document.querySelectorAll('.number')];
 
-// Iterating over the items
-items.forEach((item) => {
-
-    console.log(item);
-
-});
-
 // updateCount - start
 const updateCount = (el) => {
 
+    console.log(el);
+
 };
 // updateCount - end
+
+// Iterating over the items
+items.forEach((item) => {
+
+    updateCount(item);
+
+});
